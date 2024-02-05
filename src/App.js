bb96aef32f7250035df9a939267e1cea26de2519
JeromeDProgrammer <jeromemorales86@live.com>
2024-02-05 10:31:53 -0800

    I am deploying this app so my friends can see it

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <>
     <h1>Hello React.js</h1>
+    <p>Hi im online and my friends can view this app.</p>
+    <ul>
+      <li>Learng to deploy react.js app on github</li>
+      <li>It's exciting</li>
+    </ul>
     </>
   );
 }
